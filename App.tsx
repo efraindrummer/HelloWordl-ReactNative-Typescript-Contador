ac406414ad96affb45706bad99f50f2428997972
Efrain May <efrain_may_75@hotmail.com>
2021-06-17 00:24:41 -0500

    inicio box object model

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+import { SafeAreaView } from 'react-native';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+//import { ContadorScreen } from './src/screens/ContadorScreen';
 //import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
+    <SafeAreaView>
+      <BoxObjectModelScreen />
+    </SafeAreaView>
     //<HolaMundoScreen />
-    <ContadorScreen />
+    //<ContadorScreen />
   )
 }
