279673239bf9ecc8c79f7372ff6148d3b1d08b3d
Efrain May <efrain_may_75@hotmail.com>
2021-06-16 19:24:50 -0500

    correciones de diseño

---
+++ src/components/Fab.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
+
+interface Props {
+    title: string
+    position?: 'br' | 'bl';
+    onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+
+    return (
+        <View 
+            style={[
+                styles.fabLocation,
+                (position === 'bl') ? styles.left : styles.right
+            ]}
+        >
+            <TouchableNativeFeedback
+                onPress={ onPress }
+                background={TouchableNativeFeedback.Ripple('black', false, 30)}
+            >
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}> {title} </Text>
+                </View>
+            </TouchableNativeFeedback>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    fabLocation: {
+        position: 'absolute',
+        bottom: 25,
+    },
+    right: {
+        right: 25,
+    },
+    left: {
+        left: 25,
+    },
+    fab: {    
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 8,
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    }
+});
--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Fab } from '../components/Fab';
 
 export const ContadorScreen = () => {
 
@@ -12,23 +13,25 @@ export const ContadorScreen = () => {
                 Contador: { contador }
             </Text>
 
-            <TouchableOpacity
-                style={styles.fabLocationBR}
-                onPress={ () => setContador(contador + 1) }
-            >
-                <View style={styles.fab}>
-                    <Text style={styles.fabText}>+1</Text>
-                </View>
-            </TouchableOpacity>
+            <Fab
+                title="+1"
+                onPress={() => setContador(contador + 1)}
+            />
+            
+            <Fab
+                title="-1"
+                position="bl"
+                onPress={() => setContador(contador - 1)}
+            />
 
-            <TouchableOpacity
+            {/* <TouchableOpacity
                 style={styles.fabLocationBL}
                 onPress={ () => setContador(contador - 1) }
             >
                 <View style={styles.fab}>
                     <Text style={styles.fabText}>-1</Text>
                 </View>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
         </View>
     )
 }
@@ -43,28 +46,4 @@ const styles = StyleSheet.create({
         top: -10,
         textAlign: 'center',
     },
-    fabLocationBR: {
-        position: 'absolute',
-        bottom: 25,
-        right: 25
-    },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
-        left: 25
-    },
-    fab: {    
-        backgroundColor: '#5856D6',
-        width: 60,
-        height: 60,
-        borderRadius: 100,
-        justifyContent: 'center'
-    },
-    fabText: {
-        color: 'white',
-        fontSize: 25,
-        fontWeight: 'bold',
-        alignSelf: 'center',
-
-    }
 });
